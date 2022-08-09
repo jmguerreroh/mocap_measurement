0b1f61725ed5fdf20be79deb4a2b1e8b5d031cf4
jmguerreroh <josemiguel.guerrero@urjc.es>
2022-08-09 13:04:51 +0200

    Update author

--- mocap_measurement/launch/tb3_simulation_launch.py
+++ mocap_measurement/launch/tb3_simulation_launch.py
@@ -11,6 +11,8 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
+# Modify by: José Miguel Guerrero Hernández
 
 """This is all-in-one launch script intended for use by nav2 developers."""
 
